4762e75c17e9b326c1baa2a527ae98ada0e8b4a7
Luke Brazil <Luke.brazil13@icloud.com>
2020-10-08 10:22:04 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import RandomUser from './components/RandomUser';
+import UserData from './UserData';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {UserData: UserData}
+  }
+  render () {
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Random User</h1>
       </header>
+      <RandomUser />
     </div>
-  );
+  )
+  }
 }
 
 export default App;
---
+++ src/components/RandomUser.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import "./RandomUser.css";
+
+
+class RandomUser extends Component {
+    render() {
+        return (
+            <div className='user-card'>
+                <p>Random User</p>
+            </div>
+        )
+    }
+}
+
+export default RandomUser;
